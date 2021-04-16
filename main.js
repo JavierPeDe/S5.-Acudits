00521db93de69cad0dd899a04e5b3a1714d6d331
javierPerez <javip107@gmail.com>
2021-04-16 10:33:07 +0200

    Exercici-1

---
+++ main.js
@@ -0,0 +1,27 @@
+//Exercici-1 (nivel-1)
+// Crear la web d'acudits, el funcionament dels quals és:
+
+// - En entrar no mostrarà cap acudit. Apareixerà el títol i el botó de següent acudit“
+
+// - En prémer el botó de “Següent acudit” es farà fetch a la API d'acudits i es mostrarà per consola l'acudit en qüestió.
+
+// Nota: En aquest exercici no és necessari maquetar la web, primer farem que funcioni per a passar a aplicar-li els estils.
+
+//Variasbles:
+const urlJoke = 'https://icanhazdadjoke.com/';
+
+//randomJoke() Funcion que utiliza la API urlJoke y muestra un chiste por consola. 
+async function randomJoke() {
+    try{
+    const jokeData = await fetch(urlJoke, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const jokeObj = await jokeData.json();
+    console.log(jokeObj.joke);
+}
+catch{
+    console.log('Se ha producido un error al llamar a ' + urlJoke);
+}
+}
